6cd4a1684cb5a97bd8118b7bb24203964cce2f34
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-10 17:07:34 +0000

    fix return of the auxiliar function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t lheight = 0, rheight = 0;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
+		return (1);
 	lheight = binary_tree_height(tree->left);
 	rheight = binary_tree_height(tree->right);
 
